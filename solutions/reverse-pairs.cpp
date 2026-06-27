a52bf23601e8f1e1f314102a42a11cd9c69d3337
Aman Tiwary <amantiwary161@gmail.com>
2026-06-27 17:32:54 +0530

    add: Reverse Pairs [leetcode]

---
+++ solutions/reverse-pairs.cpp
@@ -0,0 +1,14 @@
+// Solved with GitSolve AI
+// Platform   : leetcode
+// Problem    : Reverse Pairs
+// URL        : https://leetcode.com/problems/reverse-pairs/
+// Difficulty : Hard
+// Language   : cpp
+// Saved at   : 2026-06-27T12:02:53.717Z
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        
+    }
+};
