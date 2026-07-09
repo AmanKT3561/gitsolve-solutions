960107e8d74a91b5c1f375d06e7e05fc6bbb9dea
Aman Tiwary <amantiwary161@gmail.com>
2026-07-10 02:00:08 +0530

    add: Painting the Walls [leetcode]

---
+++ solutions/painting-the-walls.cpp
@@ -0,0 +1,39 @@
+// Solved with GitSolve AI
+// Platform   : leetcode
+// Problem    : Painting the Walls
+// URL        : https://leetcode.com/problems/painting-the-walls/
+// Difficulty : Hard
+// Language   : cpp
+// Saved at   : 2026-07-09T20:30:07.569Z
+
+class Solution {
+public:
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+
+        int n = cost.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+
+        auto solve = [&](auto &&self, int idx, int remain) -> int {
+
+            if (remain <= 0)
+                return 0;
+
+            if (idx == n)
+                return 1e9;
+
+            int &memo = dp[idx][remain];
+            if (memo != -1)
+                return memo;
+
+            int take = cost[idx] +
+                       self(self, idx + 1,
+                            max(0, remain - time[idx] - 1));
+
+            int skip = self(self, idx + 1, remain);
+
+            return memo = min(take, skip);
+        };
+
+        return solve(solve, 0, n);
+    }
+};
